a4a75b28fe7e305c788ab645fa24e496a7e06f74
Acabrera31 <Acabrera_@outlook.com>
2020-02-04 18:35:32 -0500

    Script

---
+++ Assets/Apple_Tree.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple_Tree_Script : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
