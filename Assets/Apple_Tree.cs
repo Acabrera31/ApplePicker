ba75555b1c487bfe3235a53b61948460ebf20cae
Acabrera31 <Acabrera_@outlook.com>
2020-02-05 01:03:40 -0500

    Added Changes

--- Assets/Apple_Tree.cs
+++ Assets/Apple_Tree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple_Tree_Script : MonoBehaviour
+public class Apple_Tree : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public GameObject applePrefab;
@@ -20,11 +20,40 @@ public class Apple_Tree_Script : MonoBehaviour
 
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+	{
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropeApple", secondsBetweenAppleDrops);
+
     }
 
     void Update()
     {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //changing direction
+
+    if (pos.x < -leftAndRightEdge)
+		{
+            speed = Mathf.Abs(speed);
+		} else if ( pos.x > leftAndRightEdge)
+		{
+            speed = -Mathf.Abs(speed);
+		} 
         
+    }
+
+    void FixedUpdate () {
+	    if (Random.value < chanceToChangeDirections)  
+        {
+            speed *= -1;
+        }
     }
 }
